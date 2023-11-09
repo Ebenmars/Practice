85622877902ca72c504260f2be6c395957f00584
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-09 10:03:11 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double dig1 = 20.00d;
+        double dig2 = 80.00d;
+
+        double total = (dig1 + dig2) * 100.00d;
+
+        total %= 40.00;
+
+        Boolean isZero = total == 0.00 ? true : false;
+
+        System.out.println(isZero);
+
+        if(!isZero){
+            System.out.println("Got some remainder");
+        }
     }
 }
