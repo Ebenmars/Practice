92b35784572363473c26002a70a56bb41fb3c73d
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-10 14:03:28 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         double dig1 = 20.00d;
         double dig2 = 80.00d;
 
+        isRemainder(dig1, dig2);
+    }
+
+    public static void isRemainder(double dig1, double dig2) {
         double total = (dig1 + dig2) * 100.00d;
 
         total %= 40.00;
@@ -16,4 +20,29 @@ public class Main {
             System.out.println("Got some remainder");
         }
     }
+
+    public  static void displayHighScorePosition(String playerName,int playerPosition){
+        System.out.println(playerName + "manged to get into position" + playerPosition + "on the high score list");
+
+    }
+
+    public int calculateHighScorePosition(int playerScore){
+        if(playerScore >= 1000){
+            return 1;
+        }
+        else if(playerScore >= 500 && playerScore < 1000){
+            return 2;
+        }
+        else if(playerScore >= 100 && playerScore < 500){
+            return 3;
+        }
+        else{
+            return 4;
+        }
+    }
+
 }
+
+
+
+
